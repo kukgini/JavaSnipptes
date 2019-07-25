c7d0df00bd043b02c89a03b82e6ee5849492bc81
kukgini <kukgini@gmail.com>
2019-07-25 22:04:47 +0900

    ...

--- src/main/java/my/PrintWriterResource.java
+++ src/main/java/my/PrintWriterResource.java
@@ -4,6 +4,11 @@ import java.io.PrintWriter;
 import java.util.function.Consumer;
 
 public class PrintWriterResource  {
+	public static void main(String[] args) {
+		PrintWriterResource.use("test.txt", x -> {
+			x.println("test");
+		});
+	}
 	
     public static void use(String fileName, Consumer<PrintWriter> consumer) {
     	try (FileWriter f = new FileWriter(fileName);
