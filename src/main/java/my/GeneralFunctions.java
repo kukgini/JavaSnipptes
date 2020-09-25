f13b60115d0c1a2c251d2b0718e58625c5a15780
kukgini <kukgini@gmail.com>
2020-09-25 16:25:33 +0900

    ...

--- src/main/java/my/GeneralFunctions.java
+++ src/main/java/my/GeneralFunctions.java
@@ -5,20 +5,6 @@ import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
 public class GeneralFunctions {
-	static public ByteBuffer stob(String s) {
-		byte[] strBytes = s.getBytes();
-        ByteBuffer buffer = ByteBuffer.allocate(strBytes.length);
-        buffer.put(strBytes);
-        buffer.flip();
-        return buffer;
-	}
-	static public void write(FileChannel channel, String s) {
-		try {
-			channel.write(stob(s));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 	static public void sleep(int millisec) {
 		try {
 			Thread.sleep(millisec);
