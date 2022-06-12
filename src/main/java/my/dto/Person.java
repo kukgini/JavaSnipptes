58aa640fb862a44490ed7b6bdae53bc087ed07df
kukgini <kukgini@gmail.com>
2022-06-12 20:12:36 +0900

    no message

--- src/main/java/my/dto/Person.java
+++ src/main/java/my/dto/Person.java
@@ -1,8 +1,14 @@
 package my.dto;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Person {
 
     private String name;
+    private Map<String,String> addresses = new HashMap<>();
 
     public String getName() {
         return name;
@@ -10,5 +16,9 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setAddress(String key, String address) {
+        addresses.put(key,address);
     }
 }
--- src/main/java/my/json/MyJson.java
+++ src/main/java/my/json/MyJson.java
@@ -3,15 +3,31 @@ package my.json;
 import com.google.gson.Gson;
 import my.dto.Person;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MyJson {
 
     public static void main(String[] args) {
         Gson gson = new Gson();
+        //object2json(gson);
+        map2json(gson);
+    }
 
+    private static void object2json(Gson gson) {
         String json = "{\"name\":\"alice\"}";
         Person person = gson.fromJson(json, Person.class);
-
+        person.setAddress("home", "My Home is ...");
         System.out.printf("name=%s%n", person.getName());
         System.out.printf("%s%n", gson.toJson(person));
+    }
+
+    private static void map2json(Gson gson) {
+        Map<String, Object> root = new HashMap<>();
+        root.put("rootKey", "rootValue");
+        Map<String, String> sub1 = new HashMap<>();
+        sub1.put("subKey1", "subVal1");
+        root.put("sub1", sub1);
+        System.out.println(gson.toJson(root).toString());
     }
 }
