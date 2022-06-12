7b168587cda1eb7263466a661f305040c7bdec28
kukgini <kukgini@gmail.com>
2022-06-12 16:00:49 +0900

    no message

--- src/main/java/my/http/MyGateway.java
+++ src/main/java/my/http/MyGateway.java
@@ -1,19 +1,22 @@
 package my.http;
 
 import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.api.ContentProvider;
 import org.eclipse.jetty.client.api.ContentResponse;
 import org.eclipse.jetty.client.api.Request;
+import org.eclipse.jetty.client.util.InputStreamContentProvider;
 import org.eclipse.jetty.client.util.StringContentProvider;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.util.ByteArrayISO8859Writer;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.URI;
+import java.nio.ByteBuffer;
 import java.util.stream.Collectors;
 
 public class MyGateway extends HttpServlet {
@@ -21,6 +24,8 @@ public class MyGateway extends HttpServlet {
     public static void main(String[] args) throws Exception {
         new MyGateway().start();
     }
+
+    HttpClient backend = new HttpClient();
 
     public void start() throws Exception {
         org.eclipse.jetty.server.Server server = new org.eclipse.jetty.server.Server();
@@ -45,9 +50,8 @@ public class MyGateway extends HttpServlet {
             URI uri = new URI(req.getRequestURI());
             System.out.printf("uri=%s%n", uri);
 
-            HttpClient client = new HttpClient();
-            client.start();
-            Request backendRequest = client.newRequest(String.format("http://127.0.0.1:8080%s", uri));
+            backend.start();
+            Request backendRequest = backend.newRequest(String.format("http://127.0.0.1:8080%s", uri));
             for (String key : req.getParameterMap().keySet()) {
                 String[] vals = req.getParameterValues(key);
                 for (String val : vals) {
@@ -55,15 +59,16 @@ public class MyGateway extends HttpServlet {
                 }
             }
             backendRequest.method(req.getMethod());
-            backendRequest.content(new StringContentProvider(req.getReader().lines().collect(Collectors.joining(System.lineSeparator()))));
+
+            InputStream frontendInputStream = req.getInputStream();
+            backendRequest.content(new InputStreamContentProvider(frontendInputStream));
             ContentResponse backendResponse = backendRequest.send();
 
-            resp.setContentType("application/json");
-            resp.setCharacterEncoding("UTF-8");
+            resp.setContentType(backendResponse.getMediaType());
 
-            PrintWriter frontendWriter = resp.getWriter();
-            frontendWriter.write(backendResponse.getContentAsString());
-
+            OutputStream frontendOutputStream = resp.getOutputStream();
+            BufferedInputStream backendResponseInputStream = new BufferedInputStream(new ByteArrayInputStream(backendResponse.getContent()));
+            backendResponseInputStream.transferTo(frontendOutputStream);
         } catch (Throwable t) {
             t.printStackTrace();
             resp.setStatus(500);
