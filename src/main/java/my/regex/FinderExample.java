131295aa2ffa1369e5fd1d0c13160a6466a08f54
kukgini <kukgini@gmail.com>
2022-06-12 19:28:20 +0900

    no message

--- src/main/java/my/regex/FinderExample.java
+++ src/main/java/my/regex/FinderExample.java
@@ -6,12 +6,12 @@ import java.util.regex.Matcher;
 
 public class FinderExample {
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
+        Scanner stdin = new Scanner(System.in);
         while (true) {
             System.out.println("Enter regex pattern:");
-            Pattern pattern = Pattern.compile(sc.nextLine());
+            Pattern pattern = Pattern.compile(stdin.nextLine());
             System.out.println("Enter text:");
-            Matcher matcher = pattern.matcher(sc.nextLine());
+            Matcher matcher = pattern.matcher(stdin.nextLine());
             boolean found = false;
             while (matcher.find()) {
                 System.out.println("I found the text "+matcher.group()+" starting at index "+
