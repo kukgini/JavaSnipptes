ac7c16aba4f11ec85d9f6e1ff6aecf1a89e1db59
kukgini <kukgini@gmail.com>
2022-06-16 22:27:56 +0900

    ...

--- src/main/java/my/helper/FileChannelResource.java
+++ src/main/java/my/helper/FileChannelResource.java
@@ -2,6 +2,7 @@ package my.helper;
 
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.function.Consumer;
@@ -10,11 +11,10 @@ public class FileChannelResource {
 	
 	public static void main(String[] args) {
 		FileChannelResource.use("test.txt", x -> {
-			write(x, "test");
+            FileChannelResource.write(x, "연습입니다.");
 		});
 	}
-	
-    public static void use(String fileName, Consumer<FileChannel> consumer) {
+    static public void use(String fileName, Consumer<FileChannel> consumer) {
     	try (RandomAccessFile stream = new RandomAccessFile(fileName, "rw");
              FileChannel channel = stream.getChannel();
         ){
@@ -23,16 +23,19 @@ public class FileChannelResource {
     		new RuntimeException(e);
     	}
     }
-	static public ByteBuffer stob(String s) {
-		byte[] strBytes = s.getBytes();
+    static public ByteBuffer stob(String contents, String encoding) throws UnsupportedEncodingException {
+		byte[] strBytes = contents.getBytes(encoding);
 		ByteBuffer buffer = ByteBuffer.allocate(strBytes.length);
 		buffer.put(strBytes);
 		buffer.flip();
 		return buffer;
 	}
-	static public void write(FileChannel channel, String s) {
+    static public void write(FileChannel channel, String contents) {
+        write(channel, contents, "UTF-8");
+    }
+    static public void write(FileChannel channel, String contents, String encoding) {
 		try {
-			channel.write(stob(s));
+			channel.write(stob(contents, encoding));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
